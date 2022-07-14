53e13d1b700bc023fe0636e30e478f57f75088c8
alikarim007 <alikarim@gmail.com>
2022-07-14 17:40:24 +0530

    problemsolution W3A3

--- Week-3/Assign3/problem.js
+++ Week-3/Assign3/problem.js
@@ -16,3 +16,8 @@ increment();
 increment();
 log();// What is logged?
 //Ans: Count is 0
+// "In the first example, are we not tracking the reference of count and only assigning its initial value to message?" - yes. The closure is tracking the reference of message, which only has the initial value of count but doesn't get updated by the log closure.
+
+
+// Here we not tracking the reference of count and only assigning its initial value to message?
+// By moving the message variable inside of the log function are we somehow telling JS to track the reference of the count value?
