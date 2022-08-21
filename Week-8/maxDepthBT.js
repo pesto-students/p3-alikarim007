13fa1b4cbfdb64f55f2539d5aeaeed36244044bb
alikarim007 <alikarim.sayed@gmail.com>
2022-08-21 16:19:41 +0530

    maxDepth81

---
+++ Week-8/maxDepthBT.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+ var maxDepth = function(root) {
+    if(!root) return 0;
+    let max = 1;
+    findDepth(root);
+    return max;
+    
+    function findDepth(root, count = 0) {
+        if (root) {
+            findDepth(root.left, count + 1);
+            findDepth(root.right, count + 1);
+        }
+        max = Math.max(max, count);
+    }
+};
